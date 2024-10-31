06c4fe8ab7659a8f5a245b2b6de23c13492fd8bb
Ayush Priyadarshi <priyadarshiayush1304@gmail.com>
2024-10-31 13:58:33 +0545

    Sign UP and custom otp - Message APP

--- MERN_NextJS_Projects/Project6_MessageApp/mstry-message/src/app/api/sign-up/route.ts
+++ MERN_NextJS_Projects/Project6_MessageApp/mstry-message/src/app/api/sign-up/route.ts
@@ -1,15 +1,87 @@
 import bcrypt from "bcryptjs";
 import dbConnect from "@/lib/dbConnect";
-import userModel from "@/models/User";
+import { NextResponse } from "next/server";
+import userModel from "../../../../models/User";
 import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 
 export async function POST(request: Request) {
   await dbConnect();
+  const { username, email, password } = await request.json();
   try {
-    const { username, email, password } = await request.json();
+    const existingUserVerifiedByUsername = await userModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingUserVerifiedByUsername) {
+      return NextResponse.json(
+        {
+          status: false,
+          message: "User name was already taken. ",
+        },
+        { status: 400 }
+      );
+    }
+    const existingUserByEmail = await userModel.findOne({ email });
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    if (existingUserByEmail) {
+      if (existingUserByEmail.isVerified) {
+        return NextResponse.json(
+          {
+            status: false,
+            message: "User already exists with this email. ",
+          },
+          { status: 400 }
+        );
+      } else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        existingUserByEmail.password = hashedPassword;
+        existingUserByEmail.verifyCode = verifyCode;
+        existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000);
+        await existingUserByEmail.save();
+      }
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiriyDate = new Date();
+      expiriyDate.setHours(expiriyDate.getHours() + 1);
+
+      const newUser = new userModel({
+        username,
+        email,
+        password: hashedPassword,
+        verifyCode,
+        verifyCodeExpiry: expiriyDate,
+        isVerified: false,
+        isAcceptingMessage: true,
+        messages: [],
+      });
+      await newUser.save();
+    }
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode
+    );
+    if (!emailResponse.success) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        { status: 500 }
+      );
+    }
+    return NextResponse.json(
+      {
+        success: true,
+        message: emailResponse.message,
+      },
+      { status: 200 }
+    );
   } catch (error) {
     console.log(`Error Registering user: ${error}`);
-    return Response.json(
+    return NextResponse.json(
       { success: false, message: "Error registering user. " },
       { status: 500 }
     );
